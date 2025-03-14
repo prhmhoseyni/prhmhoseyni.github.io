a5dd04350798cb9f1e3d2819ab7e6aaee23b373c
prhmhoseyni <prhmhoseyni@gmail.com>
2025-03-14 07:46:10 +0330

    feat: config eslint & prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,80 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import react from "eslint-plugin-react";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const config = [
+    ...compat.extends(
+        "next/core-web-vitals",
+        "next/typescript",
+        "prettier",
+        "plugin:react/recommended",
+        "plugin:prettier/recommended"
+    ),
+    {
+        plugins: {
+            react,
+            prettier,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+
+            parser: tsParser,
+            ecmaVersion: "latest",
+            sourceType: "module",
+
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+
+        rules: {
+            "prettier/prettier": [
+                "error",
+                {
+                    tabWidth: 4,
+                    useTabs: false,
+                    semi: true,
+                    singleQuote: false,
+                    trailingComma: "es5",
+                    bracketSpacing: true,
+                    jsxBracketSameLine: false,
+                    arrowParens: "always",
+                    printWidth: 128,
+                },
+            ],
+
+            quotes: ["error", "double"],
+            "no-console": "warn",
+            "react/react-in-jsx-scope": "off",
+            "react-hooks/rules-of-hooks": "error",
+            "react-hooks/exhaustive-deps": "warn",
+        },
+    },
 ];
 
-export default eslintConfig;
+export default config;
