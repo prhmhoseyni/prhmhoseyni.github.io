df12a4eddd6d9a5ad2329039ccea392420132474
Parham Hoseyni <150419715+prhmhoseyni@users.noreply.github.com>
2025-04-15 13:28:25 +0330

    feat: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const config: NextConfig = {
     output: "export",
     trailingSlash: true,
-    httpAgentOptions: {
-        keepAlive: true,
-    },
 };
 
 export default config;
