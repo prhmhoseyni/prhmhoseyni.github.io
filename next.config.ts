2c0bee8015f841425bf27511ac32c760650a8b6d
Parham Hoseyni <150419715+prhmhoseyni@users.noreply.github.com>
2025-04-15 08:55:58 +0330

    feat: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from "next";
 
 const config: NextConfig = {
     output: "export",
-    basePath: "/portfolio",
-    assetPrefix: "/portfolio/",
     trailingSlash: true,
+    httpAgentOptions: {
+        keepAlive: true,
+    },
 };
 
 export default config;
